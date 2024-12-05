49a2273f7a8b81349f274d4a61c6607b0c29de28
David Nelson <david.nelson@unah.hn>
2024-12-05 13:07:13 -0600

    crear rutas por excel

--- src/PruebaRutas/PruebaRutas.jsx
+++ src/PruebaRutas/PruebaRutas.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import * as XLSX from "xlsx";
+import { sitios } from "./json";
 
 function PruebaRutas() {
   const handleFileUpload = (e) => {
@@ -14,33 +15,37 @@ function PruebaRutas() {
       const sheet = workbook.Sheets[sheetName];
       const data = XLSX.utils.sheet_to_json(sheet, { header: 1 });
 
-      for (let i = 2; i < data.length; ) {
+      for (let i = 3; i < data.length; ) {
+        // comienza en 2 porque ahi comienzan las rutas o sea en la fila 3 del excel
         let nombreRuta = "";
+        let nombreRutaHija = "";
+        let segmentosRutaHija = [];
+        let sitiosAnalisisHija = [];
         let sitiosAnalisis = [];
         let segmentos = [];
         let jsonRutasSimples = {};
 
         if (data[i][2] === "SIMPLE") {
-          nombreRuta = data[i + 2][2] + "/" + data[i + 2][4];
-          sitiosAnalisis.push(data[i + 4][2], data[i + 4][4]);
-          segmentos.push(data[i + 5][2], data[i + 5][4]);
+          nombreRuta = data[i + 2][2] + "/" + data[i + 2][4]; //i+2 es la fila donde estan los nombres de los sitios
+          sitiosAnalisis.push(data[i + 4][2], data[i + 4][4]); //i+4 es la fila donde estan los ids de analisis de red
+          segmentos.push(data[i + 5][2], data[i + 5][4]); // i+5 es la fila donde estan los ids de segmentos
 
           // Validaciones para sitios y segmentos adicionales
-          if (data[i + 2][6] !== undefined) {
+          if (data[i + 2][6] !== undefined) { // si hay un tercer sitio
             nombreRuta += "/" + data[i + 2][6];
             sitiosAnalisis.push(data[i + 4][6]);
             if (data[i + 5][6] !== undefined) {
               segmentos.push(data[i + 5][6]);
             }
           }
-          if (data[i + 2][8] !== undefined) {
+          if (data[i + 2][8] !== undefined) { // si hay un cuarto sitio
             nombreRuta += "/" + data[i + 2][8];
             sitiosAnalisis.push(data[i + 4][8]);
             if (data[i + 5][8] !== undefined) {
               segmentos.push(data[i + 5][8]);
             }
           }
-          if (data[i + 2][10] !== undefined) {
+          if (data[i + 2][10] !== undefined) { // si hay un quinto sitio
             nombreRuta += "/" + data[i + 2][10];
             sitiosAnalisis.push(data[i + 4][10]);
             if (data[i + 5][10] !== undefined) {
@@ -61,9 +66,9 @@ function PruebaRutas() {
             Orden: index + 1,
           }));
           jsonRutasSimples.RutaCompuesta = [null];
-          jsonRutasSimples.CreatedBy = data[2][0];
-          jsonRutasSimples.ClienteId = data[0][1];
-          jsonRutasSimples.ClienteNombre = data[1][1];
+          jsonRutasSimples.CreatedBy = data[2][0]; // fila 3 columna A
+          jsonRutasSimples.ClienteId = data[0][1]; // fila 1 columna B
+          jsonRutasSimples.ClienteNombre = data[1][1]; // fila 2 columna B
           jsonRutasSimples.Doccertificado = true;
           jsonRutasSimples.AforoRuta = true;
           jsonRutasSimples.TotalGalones = 0;
@@ -71,22 +76,127 @@ function PruebaRutas() {
           jsonRutasSimples.Clasificacion = "string";
 
           rutasJson.push(jsonRutasSimples);
-        }else{
-            alert("Papa, no estas poniendo si es simple o compuesta en la fila" + (i+1) + " " + "columna C, arregla esa onda y probá otra vez")
+        } else if (data[i][2] === "COMPUESTA") {
+          nombreRuta = data[i + 2][2] + "/" + data[i + 2][4];
+          sitiosAnalisis.push(data[i + 4][2], data[i + 4][4]);
+          segmentos.push(data[i + 5][2], data[i + 5][4]);
+
+          if (data[i][6] === undefined && data[i][8] === undefined) {
+            alert('La ruta compuesta debe tener al menos un corte');
             return;
+          }
+          if (data[i][6] === "CORTE") { // si hay corte en el tercer sitio
+              nombreRuta += "/" + data[i + 2][6];
+              sitiosAnalisis.push(data[i + 4][6]);
+              nombreRuta += "/" +  data[i + 2][8]; // despues del corte tiene que haber un sitio, entonces se agrega
+              nombreRutaHija = data[i + 2][6];
+              nombreRutaHija += "/" + data[i + 2][8];
+              //agrego a la ruta hija el id analisis del corte y el id analisis del sitio siguiente
+              sitiosAnalisisHija.push(data[i + 4][6]);
+              sitiosAnalisisHija.push(data[i + 4][8]);
+              //agrego a la ruta hija el segmento del corte y el segmento del sitio siguiente
+              segmentosRutaHija.push(data[i + 5][6]);
+              if (data[i+2][10] !== undefined) { // si hay un cuarto sitio, después puedo poner mas sitios
+                nombreRuta += "/" + data[i + 2][8];
+                nombreRutaHija += "/" + data[i + 2][10];
+                sitiosAnalisisHija.push(data[i + 4][10]);
+                segmentosRutaHija.push(data[i + 5][10]);
+              }
+          }
+
+          if (data[i][8] === "CORTE") { // si hay corte
+            nombreRuta += "/" + data[i + 2][8];
+            sitiosAnalisis.push(data[i + 4][8]);
+            nombreRuta += "/" + data[i + 2][10];
+            nombreRutaHija = data[i + 2][8];
+            nombreRutaHija += "/" + data[i + 2][10];
+
+            //agrego a la ruta hija el id analisis del corte y el id analisis del sitio siguiente
+            sitiosAnalisisHija.push(data[i + 4][8]);
+            sitiosAnalisisHija.push(data[i + 4][10]);
+
+            //agrego a la ruta hija el segmento del corte y el segmento del sitio siguiente
+            segmentosRutaHija.push(data[i + 5][8]);
+            if (data[i+2][12] !== undefined) { // si hay un quinto sitio, después puedo poner mas sitios
+              nombreRuta += "/" + data[i + 2][10];
+              nombreRutaHija += "/" + data[i + 2][12];
+              sitiosAnalisisHija.push(data[i + 4][12]);
+              segmentosRutaHija.push(data[i + 5][12]);
+            }
         }
 
-        i += 7;
+        jsonRutasSimples.TipoDeclaracionId = 6;
+          jsonRutasSimples.NombreRuta = nombreRuta;
+          jsonRutasSimples.Sitios = sitiosAnalisis.map((sitio) => ({
+            Id: sitio,
+            Regimen: "N/A",
+            DuracionEntrada: 0,
+            DuracionSalida: 0,
+            SitioJump: false,
+          }));
+          jsonRutasSimples.Segmentos = segmentos.map((segmento, index) => ({
+            Id: segmento,
+            Orden: index + 1,
+          }));
+          jsonRutasSimples.RutaCompuesta = [ //RUTA HIJA
+            {
+              TipoDeclaracionId: 0,
+              NombreRuta: nombreRutaHija,
+              Sitios: sitiosAnalisisHija.map((sitio) => ({
+                Id: sitio,
+                Regimen: "N/A",
+                DuracionEntrada: 0,
+                DuracionSalida: 0,
+                SitioJump: false,
+              })),
+              Segmentos: segmentosRutaHija.map((segmento, index) => ({
+                Id: segmento,
+                Orden: index + 1,
+              })),
+              RutaCompuesta: [null],
+              CreatedBy: data[2][0], // fila 3 columna A
+              ClienteId: data[0][1], // fila 1 columna B
+              ClienteNombre: data[1][1], // fila 2 columna B
+              Doccertificado: true,
+              AforoRuta: true,
+              TotalGalones: 0,
+              PorcentajeDesviacion: 0,
+              Clasificacion: "string",
+            },
+          ];
+          jsonRutasSimples.CreatedBy = data[2][0]; // fila 3 columna A
+          jsonRutasSimples.ClienteId = data[0][1]; // fila 1 columna B
+          jsonRutasSimples.ClienteNombre = data[1][1]; // fila 2 columna B
+          jsonRutasSimples.Doccertificado = true;
+          jsonRutasSimples.AforoRuta = true;
+          jsonRutasSimples.TotalGalones = 0;
+          jsonRutasSimples.PorcentajeDesviacion = 0;
+          jsonRutasSimples.Clasificacion = "string";
+
+          rutasJson.push(jsonRutasSimples);
+         
+        } else {
+          alert(
+            "Papa, no estas poniendo si es simple o compuesta en la fila" +
+              (i + 1) +
+              " " +
+              "columna C, arregla esa onda y probá otra vez"
+          );
+          return;
+        }
+        i += 8;
       }
 
       const contenido = rutasJson
         .map((ruta) => JSON.stringify(ruta, null, 2))
-        .join("\n******************************************************************\n");
-      const blob = new Blob([contenido], { type: "application/json" });
+        .join(
+          "\n******************************************************************\n"
+        );
+      const blob = new Blob([contenido], { type: "application/txt" });
       const url = URL.createObjectURL(blob);
       const link = document.createElement("a");
       link.href = url;
-      link.download = "rutas.json";
+      link.download = "rutas.txt";
       link.click();
       URL.revokeObjectURL(url);
     };
@@ -95,7 +205,14 @@ function PruebaRutas() {
   };
 
   return (
-    <div style={{width:"50%", margin:"200px auto", border:"2px solid #bdbdbd",padding:"50px", borderRadius:"3px"}}>
+    <div
+      style={{
+        width: "50%",
+        margin: "200px auto",
+        border: "2px solid #bdbdbd",
+        padding: "50px",
+        borderRadius: "3px",
+      }}>
       <h2>Ingresá el archivo excel chele</h2>
       <input type="file" accept=".xlsx, .xls" onChange={handleFileUpload} />
     </div>
