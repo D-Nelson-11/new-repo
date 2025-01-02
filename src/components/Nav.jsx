a7860d0de380f44687554b4ff9cc16f774316333
David Nelson <david.nelson@unah.hn>
2025-01-02 08:30:11 -0600

    cambio de rutas

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -25,7 +25,6 @@ function BarraNavegacion() {
             <Nav.Link href="/new-repo/#/Matriz">Matriz</Nav.Link>
             <Nav.Link href="/new-repo/#/Sku">Skus</Nav.Link>
             <Nav.Link href="/new-repo/#/Personas">Sitios</Nav.Link>
-
           </Nav>
         </Container>
       </Navbar>
